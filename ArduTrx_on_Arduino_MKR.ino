141625b8da654848ea55e7e22afba6ba9ea72498
Bernhard Mayer <bernhard@generationmake.de>
2020-01-17 09:21:01 +0100

    added timer interrupt

--- ArduTrx_on_Arduino_MKR.ino
+++ ArduTrx_on_Arduino_MKR.ino
@@ -156,6 +156,33 @@ int sel=0;  // pointer for menu
 byte menu_in=0;  // in menu?
 unsigned long last_settings_update;   // variable to store when setting were changed last.
 
+// Set timer TC4 to call the TC4_Handler every 5ms: (8 * (29999 + 1)) / 48MHz = 5ms
+void init_timer() {
+  PORT->Group[PORTA].DIRSET.reg = PORT_PA21;               // Set D7 as a digital output
+ 
+  GCLK->CLKCTRL.reg = GCLK_CLKCTRL_CLKEN |                 // Enable GCLK0 for TC4 and TC5
+                      GCLK_CLKCTRL_GEN_GCLK0 |             // Select GCLK0 at 48MHz
+                      GCLK_CLKCTRL_ID_TC4_TC5;             // Feed GCLK0 output to TC4 and TC5
+  while (GCLK->STATUS.bit.SYNCBUSY);                       // Wait for synchronization
+ 
+  TC4->COUNT16.CC[0].reg = 29999;                          // Set the TC4 CC0 register as the TOP value in match frequency mode
+  while (TC4->COUNT16.STATUS.bit.SYNCBUSY);                // Wait for synchronization
+
+  NVIC_SetPriority(TC4_IRQn, 0);    // Set the Nested Vector Interrupt Controller (NVIC) priority for TC4 to 0 (highest)
+  NVIC_EnableIRQ(TC4_IRQn);         // Connect TC4 to Nested Vector Interrupt Controller (NVIC)
+
+  TC4->COUNT16.INTENSET.reg = TC_INTENSET_OVF;             // Enable TC4 overflow (OVF) interrupts
+ 
+  TC4->COUNT16.CTRLA.reg |= TC_CTRLA_PRESCSYNC_PRESC |     // Reset timer on the next prescaler clock
+                            TC_CTRLA_PRESCALER_DIV8 |      // Set prescaler to 8, 48MHz/8 = 6MHz
+                            TC_CTRLA_WAVEGEN_MFRQ |        // Put the timer TC4 into match frequency (MFRQ) mode
+                            TC_CTRLA_MODE_COUNT16;         // Set the timer to 16-bit mode     
+  while (TC4->COUNT16.STATUS.bit.SYNCBUSY);                // Wait for synchronization
+
+  TC4->COUNT16.CTRLA.bit.ENABLE = 1;                       // Enable the TC4 timer
+  while (TC4->COUNT16.STATUS.bit.SYNCBUSY);                // Wait for synchronization
+}
+
 // read the buttons
 int read_LCD_buttons()
 {
@@ -646,6 +673,7 @@ void setup()
 //enable interrupts
 //  Timer1.initialize(1000);  // activate timer with 1 ms
 //  Timer1.attachInterrupt(int_timer1);
+  init_timer();
 }
  
 void loop()
@@ -929,8 +957,16 @@ void loop()
 }
 
 //void int_timer1(struct tc_module *const module_inst) // handle timer interrupt for encoder
-void int_timer1() // handle timer interrupt for encoder
-{
+void TC4_Handler()                                         // Interrupt Service Routine (ISR) for timer TC4
+{     
+  //if (TC4->COUNT16.INTFLAG.bit.OVF && TC4->COUNT16.INTENSET.bit.OVF)       // Optionally check for overflow (OVF) interrupt     
+  //{   
+    PORT->Group[PORTA].OUTTGL.reg = PORT_PA21;             // Toggle the D7 output
+    TC4->COUNT16.INTFLAG.reg = TC_INTFLAG_OVF;             // Clear the OVF interrupt flag
+  //}
+//}
+//void int_timer1() // handle timer interrupt for encoder
+//{
   static int encoder0PinALast = HIGH;
   static int encoder0PinBLast = HIGH;
   int na = LOW;
